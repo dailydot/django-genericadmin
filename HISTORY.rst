93516d4276f0376466c1d5f18ef79f89b3def6a1
Jason Novinger <jnovinger@gmail.com>
2015-11-19 11:23:48 -0600

    Create changelog and bump version to 0.6.2-alpha1

---
+++ HISTORY.rst
@@ -0,0 +1,11 @@
+.. default-domain:: name
+
+.. :changelog:
+
+History
+-------
+
+0.6.2-alpha1 (2015-11-19)
+++++++++++++++++++
+
+* Import Django 1.8+ improvements from @blag and Leo Trubach
--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@
 from setuptools import setup
 from subprocess import call
 
+
 def convert_readme():
     try:
-        call(["pandoc", "-f", "markdown_github", "-t",  "rst", "-o",  "README.txt", "README.markdown"])
+        call(["pandoc", "-f", "markdown_github", "-t",  "rst", "-o",
+              "README.txt", "README.markdown"])
     except OSError:
         pass
     return open('README.txt').read()
 
 setup(
     name='django-genericadmin',
-    version='0.6.1',
+    version='0.6.2-alpha1',
     description="Adds support for generic relations within Django's admin interface.",
     author='Weston Nielson, Jan Schrewe',
     author_email='wnielson@gmail.com, jschrewe@googlemail.com',
     url='https://github.com/jschrewe/django-genericadmin',
-    packages = ['genericadmin'],
+    packages=['genericadmin'],
 #    package_data={'genericadmin': ['static/genericadmin/js/genericadmin.js']},
     classifiers=[
         'Development Status :: 3 - Alpha',
